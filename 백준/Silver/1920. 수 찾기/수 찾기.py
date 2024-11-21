58cacee41374c83976af5a72643c7a5ce19cbaf8
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-11-21 21:46:55 +0900

    [Silver IV] Title: 수 찾기, Time: 140 ms, Memory: 51536 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,7 +1,7 @@
 import sys
-i=sys.stdin.readline
-i()
-a=set(map(int,i().split()))
-i()
-for b in map(int,i().split()):
-    print(1 if b in a else 0)
+input=sys.stdin.readline
+input()
+l=set(input().split())
+input()
+for i in input().split():
+    print(1 if i in l else 0)
