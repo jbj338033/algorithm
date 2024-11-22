15714c5df28d34dcbb1751eb08beed48fece82f1
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-11-22 09:51:28 +0900

    [Silver V] Title: 숫자 카드, Time: 656 ms, Memory: 123800 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,12 +1,9 @@
-n=int(input())
-nl=list(map(int,input().split()))
-m=int(input())
-ml=list(map(int,input().split()))
+import sys
+input=sys.stdin.readline
+input()
 d={}
-for i in ml:
-    d[i]=0
-for i in nl:
-    if i in d:
-        d[i]=1
-for i in d:
-    print(d[i],end=' ')
+for i in input().split():
+    d[i]=1
+input()
+for i in input().split():
+    print(d.get(i,0),end=' ')
