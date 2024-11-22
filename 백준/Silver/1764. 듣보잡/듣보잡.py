9169814f8af73eade0b7673ebf5bf5314b832933
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-11-22 09:42:48 +0900

    [Silver IV] Title: 듣보잡, Time: 84 ms, Memory: 44228 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -1,15 +1,8 @@
-n, m = map(int, input().split())
-
-a = set()
-b = set()
-
-for _ in range(n):
-      a.add(input())
-
-for _ in range(m):
-      b.add(input())
-
-l = sorted(list(a & b))
-
-print(len(l))
-print(*l, sep="\n")
+import sys
+input=lambda:sys.stdin.readline().rstrip()
+n,m=map(int,input().split())
+l=set([input() for _ in range(n)])
+r=set([input() for _ in range(m)])
+i=l & r
+print(len(i))
+print(*sorted(i),sep='\n')
