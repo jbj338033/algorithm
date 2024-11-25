69fcfc256d998650a55bdf6fe9f04b431fac8331
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-11-25 14:03:58 +0900

    [Silver IV] Title: 스택 2, Time: 1152 ms, Memory: 71112 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -1,16 +1,16 @@
 import sys
-stack=[]
+input=sys.stdin.readline
+s=[]
 for _ in range(int(input())):
-    s=sys.stdin.readline().split()
-    c=int(s[0])
-    
-    if c == 1:
-        stack.append(int(s[1]))
-    elif c == 2:
-        print(stack.pop() if stack else -1)
-    elif c == 3:
-        print(len(stack))
-    elif c == 4:
-        print(0 if stack else 1)
-    else:
-        print(stack[-1] if stack else -1)
+    l=list(map(int,input().split()))
+    c=l[0]
+    if c==1:
+        s.append(l[1])
+    elif c==2:
+        print(s.pop() if s else -1)
+    elif c==3:
+        print(len(s))
+    elif c==4:
+        print(0 if s else 1)
+    elif c==5:
+        print(s[-1] if s else -1)
