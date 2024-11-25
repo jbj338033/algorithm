e78e1d79f1818d6bc81761d667363b49107e6454
전민오 <82754696+jbj338033@users.noreply.github.com>
2024-11-25 14:11:37 +0900

    [Silver IV] Title: 덱 2, Time: 1348 ms, Memory: 70596 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.py
+++ 백준/Silver/28279. 덱 2/덱 2.py
@@ -1,25 +1,23 @@
-from collections import deque
 import sys
-
-d=deque()
-
+from collections import deque
+input=sys.stdin.readline
+q=deque()
 for _ in range(int(input())):
-    s=sys.stdin.readline().split()
-    c=int(s[0])
-    
-    if c == 1:
-        d.appendleft(s[1])
-    elif c == 2:
-        d.append(s[1])
-    elif c == 3:
-        print(d.popleft() if d else -1)
-    elif c == 4:
-        print(d.pop() if d else -1)
-    elif c == 5:
-        print(len(d))
-    elif c == 6:
-        print(int(not bool(d)))
-    elif c == 7:
-        print(d[0] if d else -1)
-    else:
-        print(d[-1] if d else -1)
+    l=list(map(int,input().split()))
+    c=l[0]
+    if c==1:
+        q.appendleft(l[1])
+    elif c==2:
+        q.append(l[1])
+    elif c==3:
+        print(q.popleft() if q else -1)
+    elif c==4:
+        print(q.pop() if q else -1)
+    elif c==5:
+        print(len(q))
+    elif c==6:
+        print(0 if q else 1)
+    elif c==7:
+        print(q[0] if q else -1)
+    elif c==8:
+        print(q[-1] if q else -1)
